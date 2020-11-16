c9564e0ca3adec4bd41ab56542a71179ae81768f
otaviokochi <otaviokochi@gmail.com>
2020-11-16 09:48:55 -0300

    criar turma e disciplina

--- src/components/disciplina/CriarDisciplina.jsx
+++ src/components/disciplina/CriarDisciplina.jsx
@@ -1,20 +1,74 @@
-import React from 'react'
-import './CriarDisciplina.css'
+import {
+  Form,
+  Input,
+  Button,
+  Select,
+  Col,
+  Row,
+  Space,
+  Modal,
+  Typography,
+  Divider
+} from 'antd';
 
-export default () =>
-  <div>
-    <div className='CriarDisciplina'>
-      <label htmlFor="nomeDisciplina">Nome da disciplina</label>
-      <input type="text" id="nomeDisciplina" className="imputDisciplina" />
-      <label htmlFor="cargaHoraria">Carga horária da disciplina</label>
-      <input type="text" id="cargaHoraria" className="imputDisciplina" />
-    </div>
-    <div className='botoesDisciplina'>
-      <div className='botaoDisciplina'>
-        <button onClick={e => alert('Ação cancelada!')}>Cancelar</button>
-      </div>
-      <div className='botaoDisciplina'>
-        <button onClick={e => alert('Disciplina cadastrada com sucesso!')}>Cadastrar</button>
-      </div>
-    </div>
-  </div>
+const { Option } = Select;
+
+export default ({ tipo = 'Disciplina' }) => {
+  const [form] = Form.useForm();
+
+  const onFinish = values => {
+    Modal.success({
+      content: `${tipo} ${values['nome-disciplina']} criado com sucesso!`,
+    });
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onFill = () => {
+    form.setFieldsValue({
+      'nome-disciplina': 'Matemática',
+      'carga-horaria': '68',
+    });
+  };
+
+  return (
+    <>
+      <Row gutter={24}>
+        <Col span={24}>
+          <Divider orientation="left" plain>
+            <Typography.Title level={2}>Cadastro de {tipo}</Typography.Title>
+          </Divider>
+        </Col>
+      </Row>
+      <Form layout='vertical' form={form} name="nome-disciplina" onFinish={onFinish}>
+        <Row gutter={24}>
+          <Col span={8}>
+            <Form.Item name="nome-disciplina" label="Nome Disciplina" rules={[{ required: true, message: 'Obrigatório' }]}>
+              <Input placeholder='Matemática' />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item name="carga-horaria" label="Carga horaria" rules={[{ required: true, message: 'Obrigatório' }]}>
+              <Input placeholder='68' />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item>
+          <Space size='small'>
+            <Button shape='round' size='large' type="primary" htmlType="submit">
+              Submit
+          </Button>
+            <Button shape='round' size='large' htmlType="button" onClick={onReset}>
+              Reset
+        </Button>
+            <Button shape='round' size='large' type="link" htmlType="button" onClick={onFill}>
+              Fill form
+        </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};

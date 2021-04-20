246f5e5e96ce23c39db28dc7167cf6cd570989e4
otaviokochi <otaviokochi@gmail.com>
2021-04-20 15:06:35 -0300

    merge aluno

--- src/components/aluno/CriarAluno.jsx
+++ src/components/aluno/CriarAluno.jsx
@@ -54,200 +54,240 @@ const CriarAluno = ({ initialValues, tipo = "Aluno" }) => {
 
     return (
         <Form
-            initialValues={initialValues}
-            layout="vertical"
-            form={form}
-            name="criar-aluno"
-            onFinish={onFinish}
+          initialValues={initialValues}
+          layout="vertical"
+          form={form}
+          name="criar-aluno"
+          onFinish={onFinish}
         >
-            <FormCard
-                tip='Preencha os dados corretamente para criar um aluno para o sistema'
-                title={'Cadastro de Aluno'}>
-                <Row gutter={24}>
-                    <Col span={8}>
-                        <Form.Item
-                            label="Nome Completo"
-                            name="nome"
-                            rules={[{ required: true, message: 'Nome é um campo obrigatório' }]}
-                        >
-                            <Input placeholder="Ex: João da Silva" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="CPF"
-                            name="cpf"
-                            rules={[{ required: true, message: 'CPF é um campo obrigatório' }]}
-                        >
-                            <Input disabled={!isNew} placeholder="Ex: 333.666.999-11" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="RG"
-                            name="rg"
-                            rules={[{ required: true, message: 'RG é um campo obrigatório' }]}
-                        >
-                            <Input disabled={!isNew} placeholder="Ex: 12.345.678-9" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="Email"
-                            name="email"
-                            rules={[{ required: false }]}
-                        >
-                            <Input placeholder="Ex: aluno@email.com" />
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Sexo"
-                            name="sexo"
-                            rules={[{ required: true, message: 'Indique o sexo do aluno' }]}
-                        >
-                            <Radio.Group onChange={onChange} value={value}>
-                                <Radio value={1}>Masculino</Radio>
-                                <Radio value={2}>Feminino</Radio>
-                            </Radio.Group>
-                        </Form.Item>
-
-                        <Row gutter={8}>
-                            <Space>
-                                <Form.Item
-                                    label="Série"
-                                    name="serie"
-                                    rules={[{ required: true, message: 'Indique a Série' }]}
-                                >
-                                    <Select disabled={!isNew} placeholder="Série" style={{ width: 120 }}>
-                                        {series.map(serie => (
-                                            <Option value={serie.id}>{serie.serie}ª Série</Option>
-                                        ))}
-                                    </Select>
-                                </Form.Item>
-                                <Form.Item
-                                    label="Turma"
-                                    name="turma"
-                                    rules={[{ required: true, message: 'Indique a Turma' }]}
-                                >
-                                    <Select disabled={!isNew} placeholder="Turma" style={{ width: 120 }}>
-                                        {turmas.map(turma => (
-                                            <Option value={turma.turma}>Turma {turma.turma}</Option>
-                                        ))}
-                                    </Select>
-                                </Form.Item>
-                                <Form.Item
-                                    label="Ano"
-                                    name="anoTurma"
-                                    rules={[{ required: true, message: 'Indique o Ano' }]}
-                                >
-                                    <Select disabled={!isNew} placeholder="Ano" style={{ width: 120 }}>
-                                        {turmas.map(turma => (
-                                            <Option value={turma.ano}>
-                                                {turma.ano}
-                                            </Option>
-                                        ))}
-                                    </Select>
-                                </Form.Item>
-                            </Space>
-                        </Row>
-                    </Col>
+          <FormCard
+            tip="Preencha os dados corretamente para criar um aluno para o sistema"
+            title={"Cadastro de Aluno"}
+          >
+            <Row gutter={24}>
+              <Col span={8}>
+                <Form.Item
+                  label="Nome Completo"
+                  name="nome"
+                  rules={[
+                    { required: true, message: "Nome é um campo obrigatório" },
+                  ]}
+                >
+                  <Input placeholder="Ex: João da Silva" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="CPF"
+                  name="cpf"
+                  rules={[
+                    { required: true, message: "CPF é um campo obrigatório" },
+                  ]}
+                >
+                  <Input disabled={!isNew} placeholder="Ex: 333.666.999-11" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="RG"
+                  name="rg"
+                  rules={[{ required: true, message: "RG é um campo obrigatório" }]}
+                >
+                  <Input disabled={!isNew} placeholder="Ex: 12.345.678-9" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item label="Email" name="email" rules={[{ required: false }]}>
+                  <Input placeholder="Ex: aluno@email.com" />
+                </Form.Item>
+    
+                <Form.Item
+                  label="Sexo"
+                  name="sexo"
+                  rules={[{ required: true, message: "Indique o sexo do aluno" }]}
+                >
+                  <Radio.Group onChange={onChange} value={value}>
+                    <Radio value={1}>Masculino</Radio>
+                    <Radio value={2}>Feminino</Radio>
+                  </Radio.Group>
+                </Form.Item>
+    
+                <Row gutter={8}>
+                  <Space>
+                    <Form.Item
+                      label="Série"
+                      name="serie"
+                      rules={[{ required: true, message: "Indique a Série" }]}
+                    >
+                      <Select
+                        disabled={!isNew}
+                        placeholder="Série"
+                        style={{ width: 120 }}
+                      >
+                        {series.map((serie) => (
+                          <Option value={serie.id}>{serie.serie}ª Série</Option>
+                        ))}
+                      </Select>
+                    </Form.Item>
+                    <Form.Item
+                      label="Turma"
+                      name="turma"
+                      rules={[{ required: true, message: "Indique a Turma" }]}
+                    >
+                      <Select
+                        disabled={!isNew}
+                        placeholder="Turma"
+                        style={{ width: 120 }}
+                      >
+                        {turmas.map((turma) => (
+                          <Option value={turma.turma}>Turma {turma.turma}</Option>
+                        ))}
+                      </Select>
+                    </Form.Item>
+                    <Form.Item
+                      label="Ano"
+                      name="anoTurma"
+                      rules={[{ required: true, message: "Indique o Ano" }]}
+                    >
+                      <Select
+                        disabled={!isNew}
+                        placeholder="Ano"
+                        style={{ width: 120 }}
+                      >
+                        {turmas.map((turma) => (
+                          <Option value={turma.ano}>{turma.ano}</Option>
+                        ))}
+                      </Select>
+                    </Form.Item>
+                  </Space>
                 </Row>
-            </FormCard>
-
-            <FormCard title={'Informações do Responsável Legal'}>
-                <Row gutter={24}>
-                    <Col span={8}>
-                        <Form.Item
-                            label="Nome do Responsável"
-                            name="nomeResponsavel"
-                            rules={[{ required: true, message: 'Informe o nome do responsável pelo aluno' }]}
-                        >
-                            <Input placeholder="Ex: Paulo da Silva" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="CPF do Responsável"
-                            name="cpfResponsavel"
-                            rules={[{ required: true, message: 'CPF é um campo obrigatório' }]}
-                        >
-                            <Input placeholder="Ex: 333.666.999-11" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="Telefone do Responsável"
-                            name="telefoneResponsavel"
-                            rules={[{ required: true, message: 'O número do Telefone é um campo obrigatório' }]}
-                        >
-                            <Input placeholder="Ex: (44) 9 8765-4321" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="Email do Responsável"
-                            name="emailResponsavel"
-                            rules={[{ required: false }]}
-                        >
-                            <Input placeholder="Ex: responsavel@email.com" />
-                        </Form.Item>
-                    </Col>
-                </Row>
-            </FormCard>
-
-            <FormCard title={'Informações sobre a Residência'}>
-                <Row gutter={24}>
-                    <Col span={8}>
-                        <Form.Item
-                            label="CEP"
-                            name="cep"
-                            rules={[{ required: true, message: 'CEP é um campo obrigatório' }]}
-                        >
-                            <Input placeholder="Ex: 123456789" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="Bairro"
-                            name="bairro"
-                            rules={[{ required: true, message: 'Bairro é um campo obrigatório' }]}
-                        >
-                            <Input placeholder="Ex: Jardim Felicidade" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="Endereço"
-                            name="endereco"
-                            rules={[{ required: true, message: 'Endereço é um campo obrigatório' }]}
-                        >
-                            <Input placeholder="Ex: Rua Primeiro de Janeiro" />
-                        </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                        <Form.Item
-                            label="Complemento"
-                            name="complemento"
-                            rules={[{ required: false }]}
-                        >
-                            <Input placeholder="Ex: Ponto de referência ou detalhe importante" />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Space>
-                    <Button shape='round' size='large' type="primary" htmlType="submit">
-                        Cadastrar
-                    </Button>
-
-                    <Button shape='round' size='large' htmlType="button" onClick={onReset}>
-                        Resetar
-                    </Button>
-
-                    <Button shape='round' size='large' type="link" htmlType="button" onClick={onFill}>
-                        Preencher Formulário
-                    </Button>
-                </Space>
-            </FormCard>
+              </Col>
+            </Row>
+          </FormCard>
+    
+          <FormCard title={"Informações do Responsável Legal"}>
+            <Row gutter={24}>
+              <Col span={8}>
+                <Form.Item
+                  label="Nome do Responsável"
+                  name="nomeResponsavel"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Informe o nome do responsável pelo aluno",
+                    },
+                  ]}
+                >
+                  <Input placeholder="Ex: Paulo da Silva" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="CPF do Responsável"
+                  name="cpfResponsavel"
+                  rules={[
+                    { required: true, message: "CPF é um campo obrigatório" },
+                  ]}
+                >
+                  <Input disabled={!isNew} placeholder="Ex: 333.666.999-11" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Telefone do Responsável"
+                  name="telefoneResponsavel"
+                  rules={[
+                    {
+                      required: true,
+                      message: "O número do Telefone é um campo obrigatório",
+                    },
+                  ]}
+                >
+                  <Input placeholder="Ex: (44) 9 8765-4321" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Email do Responsável"
+                  name="emailResponsavel"
+                  rules={[{ required: false }]}
+                >
+                  <Input placeholder="Ex: responsavel@email.com" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </FormCard>
+    
+          <FormCard title={"Informações sobre a Residência"}>
+            <Row gutter={24}>
+              <Col span={8}>
+                <Form.Item
+                  label="CEP"
+                  name="cep"
+                  rules={[
+                    { required: true, message: "CEP é um campo obrigatório" },
+                  ]}
+                >
+                  <Input placeholder="Ex: 123456789" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Bairro"
+                  name="bairro"
+                  rules={[
+                    { required: true, message: "Bairro é um campo obrigatório" },
+                  ]}
+                >
+                  <Input placeholder="Ex: Jardim Felicidade" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Endereço"
+                  name="endereco"
+                  rules={[
+                    { required: true, message: "Endereço é um campo obrigatório" },
+                  ]}
+                >
+                  <Input placeholder="Ex: Rua Primeiro de Janeiro" />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  label="Complemento"
+                  name="complemento"
+                  rules={[{ required: false }]}
+                >
+                  <Input placeholder="Ex: Ponto de referência ou detalhe importante" />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Space>
+              <Button shape="round" size="large" type="primary" htmlType="submit">
+                Cadastrar
+              </Button>
+    
+              <Button
+                shape="round"
+                size="large"
+                htmlType="button"
+                onClick={onReset}
+              >
+                Resetar
+              </Button>
+    
+              <Button
+                shape="round"
+                size="large"
+                type="link"
+                htmlType="button"
+                onClick={onFill}
+              >
+                Preencher Formulário
+              </Button>
+            </Space>
+          </FormCard>
         </Form>
     );
 };
